db9fa2c11ffedfc8f54ae83d464dd6152cf81ba9
Gyromitte <gyromitte@gmail.com>
2021-09-24 18:58:49 -0500

    Now you can draw when the mouse hovers one of the squares in the grid

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ let gridSize = 16;
 
 const container = document.querySelector('#container');
 
-gridArea = gridSize * gridSize;
-function createGrid(gridSize){
 
-    //Make horizontal divs
+gridArea = gridSize * gridSize;
+
+function createGrid(gridSize){
     for(i=0; i < gridArea; i++){
         const content = document.createElement('div');
-        content.classList.add('square', 'horizontal-line');
+        content.classList.add('square');
         container.appendChild(content);
     }
-   
 }
 createGrid(gridSize);
+
+const square = document.querySelectorAll('.square').forEach(square =>{
+    square.addEventListener('mouseover', e=>{
+        square.style.background = "black";
+    });
+});
+
+
+
