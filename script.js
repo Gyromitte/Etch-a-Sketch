d3059901079287d7a6f697a1c80bc9f92d6e810e
Gyromitte <gyromitte@gmail.com>
2021-09-24 19:29:50 -0500

    Added a clear button

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let gridSize = 16;
 
 const container = document.querySelector('#container');
-
-
-gridArea = gridSize * gridSize;
+const clearButton = document.querySelector('#clearButton');
 
 function createGrid(gridSize){
+    gridArea = gridSize * gridSize;
     for(i=0; i < gridArea; i++){
         const content = document.createElement('div');
         content.classList.add('square');
@@ -18,6 +17,14 @@ const square = document.querySelectorAll('.square').forEach(square =>{
     square.addEventListener('mouseover', e=>{
         square.style.background = "black";
     });
+});
+
+clearButton.addEventListener('click', e=>{
+    const square = document.querySelectorAll('.square').forEach(square =>{
+            square.style.background = "white";
+    });
+    var gridSize= prompt("Write the pixels of the new grid: ");
+    createGrid(gridSize);
 });
 
 
